9ef4c3192965953e5662eb50c4c93a1d466c4595
samfedenia <sam@fedenia.xyz>
2022-05-05 16:41:05 -0500

    add dark mode toggle

---
+++ pages/_document.tsx
@@ -0,0 +1,29 @@
+// pages/_document.js
+import React from "react";
+import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document';
+
+type Props = {}
+
+export default class MyDocument extends Document<Props> {
+    static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+
+    return initialProps
+    }
+    render() {
+        return (
+            <Html className='gray-50 dark:bg-black'>
+            <Head>
+                <meta name='description' content='Calculate your upcoming Ethereum consensus duties' />
+                <link rel='icon' href='/favicon.ico' />
+                <link rel='stylesheet' href='https://rsms.me/inter/inter.css' />
+                <script defer src='/noflash.js' />
+            </Head>
+            <body className='gray-50 dark:bg-black'>
+                <Main />
+                <NextScript />
+            </body>
+            </Html>
+        );
+    }
+}
