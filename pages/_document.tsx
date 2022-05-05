6153176028908569b3802da4065b709e420fc7a1
samfedenia <sam@fedenia.xyz>
2022-05-05 17:50:55 -0500

    fix noFlash.js

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-sync-scripts */
 // pages/_document.js
 import React from "react";
 import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document';
@@ -17,7 +18,7 @@ export default class MyDocument extends Document<Props> {
                 <meta name='description' content='Calculate your upcoming Ethereum consensus duties' />
                 <link rel='icon' href='/favicon.ico' />
                 <link rel='stylesheet' href='https://rsms.me/inter/inter.css' />
-                <script defer src='/noflash.js' />
+                <script src='noflash.js' />
             </Head>
             <body className='gray-50 dark:bg-black'>
                 <Main />
