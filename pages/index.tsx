223065860b88f51b7248df025a2e118f747fa54b
samfedenia <sam@fedenia.xyz>
2022-05-05 19:35:11 -0500

    added header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,24 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import ToggleDarkMode from '../components/theme/ToggleDarkMode'
-import InputField from '../components/theme/input/InputField'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Header from '../components/Header';
+import ToggleDarkMode from '../components/theme/ToggleDarkMode';
+import InputField from '../components/input/InputField';
 
 const Home: NextPage = () => {
   return (
-       <div className="bg-inherit w-screen h-screen flex flex-col justify-start items-center space-y-4">
+    <div className='bg-inherit w-screen h-screen flex flex-col justify-start items-center space-y-4'>
       <Head>
         <title>Validator Duties</title>
       </Head>
-      <ToggleDarkMode/>
-      <h1 className="dark:text-white text-5xl font-bold">Hello world</h1>
-      <InputField/>
-    </div>
-  )
-}
+      <ToggleDarkMode />
+      <Header
+        title='Ethereum Validator Duties'
+        subtitle='View upcoming attestations and block proposals'
+      />
 
-export default Home
+      <InputField />
+    </div>
+  );
+};
+
+export default Home;
